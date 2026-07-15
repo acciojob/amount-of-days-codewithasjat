2bf257cb82839aef17ffae5b9f04a054c23cc5c1
codewithasjat <128043755+codewithasjat@users.noreply.github.com>
2026-07-15 12:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear (year){
+
+	if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366
+} else {
+    return 365
+}
+	
+	
+}
+
+console.log(daysOfAYear(2022))
+
